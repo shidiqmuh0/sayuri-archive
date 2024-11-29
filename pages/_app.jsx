43cc0a55f8fd6339333bf809bed987fbb17e2093
shidiq <57977381+shidiqmuh0@users.noreply.github.com>
2024-11-29 04:09:47 +0000

    add google analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,3 +1,43 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import Script from 'next/script';
+
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
-  }
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag('config', 'G-RQLP7E1FTL', {
+        page_path: url,
+      });
+    };
+
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
+  return (
+    <>
+      {/* Google Analytics Script */}
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-RQLP7E1FTL"
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-RQLP7E1FTL');
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  );
+}
